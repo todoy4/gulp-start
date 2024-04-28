8ef67319603f5ea2552448e098bb08211d2d0ebf
todoy4 <garik.potter2006@gmail.com>
2024-04-28 17:31:22 +0300

    js

--- source/js/script.js
+++ source/js/script.js
@@ -1,17 +1,43 @@
-const openMenu = document.querySelector('.header__button-open');
-const closedMenu = document.querySelector('.header__button-closed');
-console.log(closedMenu);
-const menu = document.querySelector('.header__nav');
+const headerOpenMenu = document.querySelector('.header__button-open');
+const headerClosedMenu = document.querySelector('.header__button-closed');
+const headerMenu = document.querySelector('.header__nav');
 
-openMenu.addEventListener('click', () => {
-   menu.classList.add('header__nav--showed');
-   closedMenu.classList.remove('header__button-closed--hidden');
-   openMenu.classList.add('header__button-open--hidden');
-   closedMenu.addEventListener('click', close);
+headerOpenMenu.addEventListener('click', () => {
+    headerMenu.classList.add('header__nav--showed');
+    headerClosedMenu.classList.remove('header__button-closed--hidden');
+    headerOpenMenu.classList.add('header__button-open--hidden');
+    headerClosedMenu.addEventListener('click', disappearanceMenu);
 });
 
-const close = () => {
-   menu.classList.remove('header__nav--showed');
-   openMenu.classList.remove('header__button-open--hidden');
-   closedMenu.removeEventListener('click', close);
-};
+const disappearanceMenu = () => {
+    headerMenu.classList.remove('header__nav--showed');
+    headerClosedMenu.classList.add('header__button-closed--hidden');
+    headerOpenMenu.classList.remove('header__button-open--hidden');
+    headerClosedMenu.addEventListener('click', disappearanceMenu);
+}
+
+
+
+
+
+const productButton = document.querySelectorAll('.product__button');
+const modal = document.querySelector('.modal');
+const modalCloseButton = document.querySelector('.modal__close-button');
+const modalResume = document.querySelector('.modal__resume');
+const shadow = document.querySelector('.shadow');
+
+productButton.forEach((button) => {
+    button.addEventListener('click', () => {
+        modal.classList.remove('modal--hidden');
+        shadow.classList.remove('shadow--hidden');
+        modalCloseButton.addEventListener('click', close);
+        modalResume.addEventListener('click', close);
+    });
+});
+
+const close = () => { 
+    modal.classList.add('modal--hidden');
+    shadow.classList.add('shadow--hidden');
+    modalResume.removeEventListener('click', close);
+    modalCloseButton.removeEventListener('click', close);  
+}
