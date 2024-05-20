67c3095737243b8638e37ed8da209f2a53dd28aa
todoy4 <garik.potter2006@gmail.com>
2024-05-20 17:48:07 +0300

    Update modal.js

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,29 +1,26 @@
 const overlay = document.querySelector('.overlay');
 
-export const closeModal = (event) => {
-    const modal = document.querySelector('.modal');
-    const modalCloseButton = modal.querySelector('.modal__close');
-    const modalResume = modal.querySelector('.modal__resume');
+const closeModal = (event) => {
+    const modals = document.querySelectorAll('.modal');
     
-    if (event.target === modalCloseButton || event.target === modalResume || event.target === overlay) {
-        modal.classList.add('modal--hidden');
-        overlay.classList.add('overlay--hidden');
-        modal.querySelector('.modal__close').removeEventListener('click', closeModal);
-    }
+    modals.forEach(modal => {
+        if(!modal.classList.contains('modal--hidden')) {
+            const modalCloseButton = modal.querySelector('.modal__close');
+   
+            if (event.target === modalCloseButton || event.target === overlay) {
+                modal.classList.add('modal--hidden');
+                modalCloseButton.querySelector('.modal__close').removeEventListener('click', closeModal);
+            }
+        } 
+    })
 };
 
-export const openModal =  (event) => {
-    const modal = document.querySelector('.modal');
+export const openModal =  (event, modal) => {
+    event.preventDefault();
     const modalCloseButton = modal.querySelector('.modal__close');
-    const modalResume = modal.querySelector('.modal__resume');
-    overlay.classList.remove('overlay--hidden');
     modal.classList.remove('modal--hidden');
     
     modalCloseButton.addEventListener('click', closeModal);
-    overlay.addEventListener('click', closeModal);
-    if (modalResume) {
-    modalResume.addEventListener('click', closeModal);
-    }
-    overlay.addEventListener('click', closeModal);
 };
 
+document.addEventListener('click', closeModal)
